8a01a0d0703ed662bc8184cd03cdfc8380b18d9a
codemattcode <codemattcode@gmail.com>
2017-01-12 01:08:42 +0000

    init

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>React ToyShop</h2>
+        </div>
+        <p className="App-intro">
+           <code>This is where the magic happens</code>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
